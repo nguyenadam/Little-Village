f1ceeb61267bd2b1361ed09341214b4922ecfba2
Adam Nguyen <nguyenadam5@yahoo.com>
2022-07-16 12:34:22 -0500

    working shop!

--- ShopCard.gd
+++ ShopCard.gd
@@ -1,7 +1,11 @@
 extends "res://Card.gd"
-
-var mouse_in = false
+signal clicked(node)
 
 func _on_Node2D_mouse_entered():
-	print("MOUSE ENTERED!")
-	pass # Replace with function body.
+	#print("MOUSE ENTERED!")
+	pass
+
+func _on_Node2D_input_event(viewport, event, shape_idx):
+	if Input.is_action_pressed("mouse_button_left"):
+		emit_signal("clicked", self)
+
